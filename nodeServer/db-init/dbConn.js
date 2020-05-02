c863f3c3f967f09eca159a2602175e55e0734735
Nikhil Elayath <nikhil.elayath13@gmail.com>
2020-05-02 23:49:31 +0530

    changed postgres url

--- nodeServer/db-init/dbConn.js
+++ nodeServer/db-init/dbConn.js
@@ -11,7 +11,11 @@ const pg = require("pg-promise")(options);
 const config = require("config");
 
 //getting the postgres url which is stored in the environment variable
-const postgresURL = config.get("postgresURL");
+const postgresURL = {
+  connectionString:
+    "postgres://aimqijgpovdocl:90f1b3a77c6354a1798bef0efef93cea31689cdf9c9b3ca2e94ec6376ad0989a@ec2-50-17-21-170.compute-1.amazonaws.com:5432/d6hnuie8qovv9s",
+  ssl: { rejectUnauthorized: false },
+};
 //passing the url
 const db = pg(postgresURL);
 
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const logger = require('morgan')
-const db = require('./db-init/dbConn')
-const error = require('./middlewares/error')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("morgan");
+const db = require("./db-init/dbConn");
+const error = require("./middlewares/error");
 
 //importing the Router files
-const onboarding = require('./routes/api/Onboarding')
-const driverLocation = require('./routes/api/DriverLocation')
+const onboarding = require("./routes/api/Onboarding");
+const driverLocation = require("./routes/api/DriverLocation");
 
-app.use(bodyParser.json())
-app.use(cors())
-app.disable('etag')
+app.use(bodyParser.json());
+app.use(cors());
+app.disable("etag");
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
-)
-app.use(logger('common'))
+);
+app.use(logger("common"));
 // using the router files
-app.use('/api/onboarding/', onboarding)
-app.use('/api/driver-location/', driverLocation)
+app.use("/api/onboarding/", onboarding);
+app.use("/api/driver-location/", driverLocation);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(error)
+app.use(error);
 
 //creating the connection to the database
 db.connect()
-  .then(obj => {
-    obj.done() // success, release connection;
-    if (process.env.NODE_ENV !== 'test')
+  .then((obj) => {
+    obj.done(); // success, release connection;
+    if (process.env.NODE_ENV !== "test")
       app.listen(port, () =>
         console.log(`Server is listening at http://localhost:${port}`)
-      )
+      );
   })
-  .catch(error => {
-    console.log('ERROR:', error.message)
-  })
+  .catch((error) => {
+    console.log("ERROR:", error.message);
+  });
 
-module.exports = app
+module.exports = app;
